b12ead65455f5bb70248c0f4eaf5558a844b3339
Tilen Komel <me@komelt.dev>
2024-10-13 12:01:34 +0200

    Unzip plastnice fix

--- export.sh
+++ export.sh
@@ -57,8 +57,8 @@ else
     # Extract plastnice data
     echo -n "Extracting plastnice data...  "
     unzip DTM_SLO_RELIEF.zip -d ./DTM_SLO_RELIEF
-    unzip "DTM_SLO_RELIEF/DTM_SLO_RELIEF_EL_PLASTNICE_ZAHOD_L_*" -d ./plastnice
-    unzip "DTM_SLO_RELIEF/DTM_SLO_RELIEF_EL_PLASTNICE_VZHOD_L_*" -d ./plastnice
+    unzip DTM_SLO_RELIEF/DTM_SLO_RELIEF_EL_PLASTNICE_ZAHOD_L_* -d ./plastnice
+    unzip DTM_SLO_RELIEF/DTM_SLO_RELIEF_EL_PLASTNICE_VZHOD_L_* -d ./plastnice
 
     if [ $? -ne 0 ]; then
         echo "Failed"
