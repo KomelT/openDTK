b49b6fb3e58142c2369019bf5788b5fa4602d6b7
Tilen Komel <me@komelt.dev>
2024-10-22 12:37:49 +0200

    Updated exportStyles.py

--- scripts/exportStyles.py
+++ scripts/exportStyles.py
@@ -1,24 +1,56 @@
-from qgis.core import QgsApplication, QgsProject, QgsSldExportContext, QgsVectorLayer
+import argparse
+import os
+from os.path import abspath
+from qgis.core import QgsApplication, QgsProject
 
 
+parser = argparse.ArgumentParser(
+    prog="exportStyles.py",
+    description="This script exports styles from QGIS project to SLD files",
+)
+parser.add_argument("-o", "--output", help="Output folder", required=True)
+parser.add_argument("-i", "--input", help="Qgis project file", required=True)
+parser.add_argument("-r", action="store_true", help="Removes output folder if exists")
+
+args = parser.parse_args()
+
+# Check if input folder exists
+if not os.path.exists(args.input):
+    print(f"ERROR: Input file '{args.input}' does not exist!")
+    exit(1)
+
+# Remove output folder if exists
+if args.r:
+    print(f"INFO: Removing output folder '{args.output}'")
+    os.system(f"rm -rf {args.output}")
+
+# Create output folder
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
+
+output_folder = abspath(args.output)
+input_file = abspath(args.input)
+
+# Init QGIS
 QgsApplication.setPrefixPath("/usr/bin/qgis", True)
 qgs = QgsApplication([], False)
 qgs.initQgis()
 
-# Load project ./dtk.qgs
+# Load project
 project = QgsProject.instance()
-project.read("dtk.qgs")
-
-print(project.mapLayers())
+project.read(input_file)
 
 # Load layers
 contours_50 = project.mapLayersByName("CONTOURS_50")[0]
 contours_10 = project.mapLayersByName("CONTOURS_10")[0]
 
-if not contours_50.isValid():
-    print("ERROR: 'Contours 50' Layer not loaded!")
+errM50, err50 = contours_50.saveSldStyle(f"{output_folder}/contours_50.sld")
+errM10, err10 = contours_10.saveSldStyle(f"{output_folder}/contours_10.sld")
+
+if err50 != True:
+    print(f"ERROR: Could not save style for contours_50: {errM50}")
     exit(1)
 
-export_context = QgsSldExportContext()
-export_context.exportFilePath = "./contours_50.sld"
-contours_50.saveSldStyleV2(export_context)
+if err10 != True:
+    print(f"ERROR: Could not save style for contours_10: {errM10}")
+    exit(1)
