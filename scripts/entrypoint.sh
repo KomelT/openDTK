1a453842ac0418783a33d9a8cc802b5a93a91f8b
Tilen Komel <me@komelt.dev>
2024-10-25 13:53:00 +0200

    Add comments

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Check dependencies
+
+#####
+## Check if all required tools are installed
+#####
 python3 ./scripts/exportStyles.py -d
 ./scripts/fetchRawLayers.sh -d
 python3 ./scripts/exportContours.py -d
@@ -34,8 +37,9 @@ then
 fi
 
 
-
-# Export styles
+#####
+## Export styles
+#####
 python3 ./scripts/exportStyles.py -i ./dtk.qgs -o ./geoserver.d/workspaces/OpenDTK/styles/
 
 # Check if styles export was successful
@@ -45,50 +49,70 @@ if [ $? -ne 0 ]; then
 fi
 
 
-# Fetch data
+#####
+## Fetch raw layers
+#####
 ./scripts/fetchRawLayers.sh ./data/orig
 
-# Check if fetch was successful
 if [ $? -ne 0 ]; then
   echo "Error while fetching data"
   exit 1
 fi
 
 
-# Convert OSM to OSM XML
+#####
+## Convert OSM to OSM XML
+#####
 echo "Converting OSM to OSM XML... "
-osmconvert ./data/orig/slovenia-latest.osm.pbf --out-osm -o=./data/orig/slovenia-latest.osm
-echo -e "Done\n"
+if [ ! -f ./data/orig/slovenia-latest.osm ]; then
+  osmconvert ./data/orig/slovenia-latest.osm.pbf --out-osm -o=./data/orig/slovenia-latest.osm
+  echo -e "Done\n"
+else
+  echo -e "OSM XML already exists\n"
+fi
 
-# Check if conversion was successful
 if [ $? -ne 0 ]; then
   echo "Error while converting OSM to OSM XML"
   exit 1
 fi
 
-# Filter OSM XML
-echo "Filtering OSM XML... "
-osmfilter ./data/orig/slovenia-latest.osm --drop-version --drop-author --keep="landuse=forest" >> ./data/orig/custom-slovenia-latest.osm
-echo -e "Done\n"
 
-# Check if filtering was successful
+#####
+## Filter OSM XML
+#####
+echo "Filtering OSM XML... "
+if [ ! -f ./data/orig/slovenia-latest.osm ]; then
+  osmfilter ./data/orig/slovenia-latest.osm --drop-version --drop-author --keep="landuse=forest" >> ./data/orig/custom-slovenia-latest.osm
+  echo -e "Done\n"
+else
+  echo -e "Filtered OSM XML already exists\n"
+fi
+
 if [ $? -ne 0 ]; then
   echo "Error while filtering OSM XML"
   exit 1
 fi
 
-# Export contours shapefiles
-python3 ./scripts/exportContours.py -r -i ./data/orig/contours/ -o ./data/out/contours
 
-# Check if export was successful
+#####
+## Export contours shapefiles
+#####
+python3 ./scripts/exportContours.py -i ./data/orig/contours/ -o ./data/out/contours
+
 if [ $? -ne 0 ]; then
   echo "Error while exporting contours"
   exit 1
 fi
 
-# Upload OSM data to PostGIS
+
+#####
+## Upload OSM data to PostGIS
+#####
 osm2pgsql -S ./scripts/compatible.lua -O flex -d postgresql://geoserver:geoserver@postgis:5432/geoserver ./data/orig/custom-slovenia-latest.osm
 
-# Upload Contours to PostGIS
+
+#####
+## Upload Contours to PostGIS
+#####
 shp2pgsql -s 3794 ./data/out/contours/contours10.shp contours10 | psql -q -d postgresql://geoserver:geoserver@postgis:5432/geoserver
 shp2pgsql -s 3794 ./data/out/contours/contours50.shp contours50 | psql -q -d postgresql://geoserver:geoserver@postgis:5432/geoserver
